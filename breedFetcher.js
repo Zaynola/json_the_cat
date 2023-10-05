cdcc7324f21ed3791c4bb39518cec88108177f9e
Zaynola <olaoluodushola@gmail.com>
2023-10-04 20:43:20 -0400

    add breedfetcher function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,25 +1,25 @@
 const request = require('request');
-const breedName = process.argv[2];
-const url = "https://api.thecatapi.com/v1/breeds/search?q=" + breedName;
 
-request(url, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-        process.exit(1);
-    }
 
-    if (response.statusCode !== 200) {
-        console.error(`HTTP request failed with status code ${response.statusCode}`);
-        process.exit(1);
-    }
+const fetchBreedDescription = (breedName, callback) => {
+    const url = "https://api.thecatapi.com/v1/breeds/search?q=" + breedName;
 
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-        console.log(`Breed '${breedName}' not found.`);
-    } else {
-        //console.log(data);
-        //console.log(typeof data);
-        const firstEntry = data[0];
-        console.log(firstEntry.description);
-    }
-});
+    request(url, (error, response, body) => {
+        if (error) {
+            callback(Error, null);
+        } else if (response.statusCode !== 200) {
+            callback(`HTTP request failed with status code ${response.statusCode}`, null);
+        } else {
+            const data = JSON.parse(body);
+            if (data.length === 0) {
+                callback(`Breed '${breedName}' not found.`, null);
+            } else {
+                const firstEntry = data[0];
+                callback(null, firstEntry.description);
+            }
+        }
+    });
+};
+
+
+module.exports = { fetchBreedDescription };
